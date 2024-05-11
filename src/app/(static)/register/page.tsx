2582d64da93aa56fd0e30d6a680fda33ced61774
anandafiqri <anandafiqri1403@gmail.com>
2024-05-11 10:28:53 +0700

    ref:separate ui & logic

--- src/app/(static)/register/page.tsx
+++ src/app/(static)/register/page.tsx
@@ -1,39 +1,8 @@
-import { Mutate } from "@/actions";
+import { registerAction } from "@/actions/user";
 import Container from "@/components/atoms/contents/container";
 import FormRegisterLogin from "@/components/forms/FormRegisterLogin";
-import { registerMutation } from "@/mutations/registerLogin";
-import { redirect } from "next/navigation";
 
 export default function RegisterPage() {
-  const registerAction = async (formData: FormData) => {
-    "use server";
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const fullname = formData.get("fullname") as string;
-    const username = formData.get("username") as string;
-    const confirmPassword = formData.get("confirmPassword") as string;
-    const role = formData.get("role") as string;
-
-    const { data, errors } = await Mutate({
-      mutation: registerMutation,
-      variables: {
-        payload: {
-          email,
-          password,
-          confirmPassword,
-          fullname,
-          username,
-          role,
-        },
-      },
-    });
-    if (!data && errors?.length) {
-      let errorMessage = errors[0].message.split("\n")[0];
-      return redirect(`/register?error=${errorMessage}`);
-    }
-
-    return redirect(`/login`);
-  };
   return (
     <Container as="section">
       <div className="flex flex-col w-full h-full items-center justify-center">
